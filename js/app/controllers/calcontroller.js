b3fcddc3877917886119dd327abbec124ddc9904
Georg Ehrke <developer@georgehrke.com>
2016-01-18 20:33:56 +0100

    show data in events editor sidebar

--- js/app/controllers/calcontroller.js
+++ js/app/controllers/calcontroller.js
@@ -172,8 +172,11 @@ app.controller('CalController', ['$scope', '$rootScope', '$window', 'CalendarSer
 								controller: 'EventsSidebarEditorController',
 								appendTo: angular.element('#app-content'),
 								resolve: {
-									event: function() {
-										return result.event;
+									fcEvent: function() {
+										return fcEvent;
+									},
+									isNew: function() {
+										return false;
 									}
 								},
 								scope: $scope
