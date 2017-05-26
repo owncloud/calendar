221bea614d9153a17aea2a506e2c8469258cabea
Georg Ehrke <developer@georgehrke.com>
2017-05-26 15:59:49 +0200

    show displayname instead of userid after creating a new user share
    Signed-off-by: Georg Ehrke <developer@georgehrke.com>

--- js/app/service/calendarService.js
+++ js/app/service/calendarService.js
@@ -519,11 +519,12 @@ app.service('CalendarService', function(DavClient, StringUtility, XMLUtility, Ca
 	 * @param {Calendar|WebCal} calendar
 	 * @param {number} shareType
 	 * @param {string} shareWith
+	 * @param {string} shareWithDisplayname
 	 * @param {boolean} writable
 	 * @param {boolean} existingShare
 	 * @returns {Promise}
 	 */
-	privateAPI.share = function(calendar, shareType, shareWith, writable, existingShare) {
+	privateAPI.share = function(calendar, shareType, shareWith, shareWithDisplayname, writable, existingShare) {
 		const [skeleton, oSetChildren] = XMLUtility.getRootSkeleton(
 			[DavClient.NS_OWNCLOUD, 'o:share'], [DavClient.NS_OWNCLOUD, 'o:set']);
 
@@ -562,17 +563,16 @@ app.service('CalendarService', function(DavClient, StringUtility, XMLUtility, Ca
 				return;
 			}
 
-			//TODO - fix displayname
 			if (shareType === SHARE_USER) {
 				calendar.shares.users.push({
 					id: shareWith,
-					displayname: shareWith,
+					displayname: shareWithDisplayname,
 					writable: writable
 				});
 			} else {
 				calendar.shares.groups.push({
 					id: shareWith,
-					displayname: shareWith,
+					displayname: shareWithDisplayname,
 					writable: writable
 				});
 			}
