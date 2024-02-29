e495e82a0a030fefdcd30c5c48945b4ea470d4a7
ownClouders <devops@owncloud.com>
2024-02-29 00:53:04 +0000

    [tx] updated from transifex

--- l10n/he.js
+++ l10n/he.js
@@ -191,4 +191,4 @@ OC.L10N.register(
     "iOS/OS X CalDAV address" : "כתובת CalDAV עבור iOS/OS X",
     "Subscribe" : "רישום"
 },
-"nplurals=4; plural=(n == 1 && n % 1 == 0) ? 0 : (n == 2 && n % 1 == 0) ? 1: (n % 10 == 0 && n % 1 == 0 && n > 10) ? 2 : 3;");
+"nplurals=3; plural=(n == 1 && n % 1 == 0) ? 0 : (n == 2 && n % 1 == 0) ? 1: 2;");
